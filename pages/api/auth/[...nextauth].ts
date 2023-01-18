56d93d004945e3f36c3f5938add719de11100af4
Gelar <gelargew@gmail.com>
2023-01-18 08:30:45 +0700

    credential provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
 import prisma from "../../../lib/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const authHandler: NextApiHandler = (req, res) =>
   NextAuth(req, res, {
@@ -11,6 +12,20 @@ const authHandler: NextApiHandler = (req, res) =>
         clientId: process.env.GITHUB_ID as string,
         clientSecret: process.env.GITHUB_SECRET as string,
       }),
+      // CredentialsProvider({
+      //   name: "credentials",
+      // credentials: {
+      //   email: {
+      //     label: "Email",
+      //     type: "email",
+      //     placeholder: "jsmith@gmail.com",
+      //   },
+      //   password: { label: "Password", type: "password" },
+      // },
+      // authorize: async (credentials, request) => {
+      //   // login logic goes here
+      // },
+      // }),
     ],
     adapter: PrismaAdapter(prisma),
     secret: process.env.SECRET,
